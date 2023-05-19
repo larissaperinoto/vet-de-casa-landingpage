9499c7a1d8a920d98bb39f3641e5c4b049eafa24
Larissa Perinoto <perinotolarissa@gmail.com>
2023-05-19 08:38:19 +0200

    feat: insere transição para card descritivo de serviço

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -16,27 +16,29 @@
       </div>
     </div>
     <div
-      class="hidden w-full h-90 bg-white absolute"
+      class="hidden w-full h-screen bg-white absolute flex items-center justify-center"
       id="serviceDescription"
     >
-      <div class="lg:w-1/2 rounded-[52px] m-auto p-5 sm:p-6 md:p-7 lg:p-8 border shadow-md">
+      <div
+        class="lg:w-1/2 bg-white m-auto p-5 sm:p-6 md:p-7 lg:p-8 md:rounded-[52px] md:border md:shadow-md"
+      >
         <div class="flex justify-start">
           <button v-on:click="hiddeContent">
             <img
               src="https://img.icons8.com/ios-filled/50/737373/delete-sign--v1.png"
               alt="Fechar"
-              class="bg-transparent rounded-none w-[20px]"
+              class="bg-transparent rounded-none w-[30px] "
             />
           </button>
         </div>
-        <div class="flex row items-center gap-x-4 justify-center">
+        <div class="flex flex-row items-center gap-x-4 justify-center">
           <img
             :src="this.serviceSelected.icon"
             class="w-[50px] p-2"
           />
           <h2>{{ this.serviceSelected.title }}</h2>
         </div>
-          <p class=" mx-auto">{{ this.serviceSelected.content }}</p>
+          <p class="mx-auto">{{ this.serviceSelected.content }}</p>
       </div>
     </div>
   </section>
